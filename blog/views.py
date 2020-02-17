79b64cc715676f7d3aefe600c74aa6359351ceae
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-02-17 21:35:12 +0300

    Optimize post serializing on tag page

--- blog/views.py
+++ blog/views.py
@@ -7,20 +7,6 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-def serialize_post(post):
-    return {
-        "title": post.title,
-        "teaser_text": post.text[:200],
-        "author": post.author.username,
-        "comments_amount": len(Comment.objects.filter(post=post)),
-        "image_url": post.image.url if post.image else None,
-        "published_at": post.published_at,
-        "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
 def serialize_post_optimized(post):
     return {
         "title": post.title,
@@ -109,12 +95,12 @@ def tag_filter(request, tag_title):
         .prefetch_related('author')[:5] \
         .fetch_with_comments_count()
 
-    related_posts = tag.posts.all().prefetch_related('author')[:20]
+    related_posts = tag.posts.all().annotate(comments_count=Count('comments')).prefetch_related('author')[:20]
 
     context = {
         "tag": tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        "posts": [serialize_post(post) for post in related_posts],
+        "posts": [serialize_post_optimized(post) for post in related_posts],
         'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
     }
     return render(request, 'posts-list.html', context)
