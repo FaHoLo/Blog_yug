7b1bd264113ebad3335f8d885a637068d8fe659a
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-02-19 19:40:06 +0300

    Prefetch cooment authors & post likes amount

--- blog/views.py
+++ blog/views.py
@@ -50,8 +50,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    post = Post.objects.annotate(likes_amount=Count('likes')).get(slug=slug)
+    comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -69,7 +69,7 @@ def post_detail(request, slug):
         "text": post.text,
         "author": post.author.username,
         "comments": serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes_amount,
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
